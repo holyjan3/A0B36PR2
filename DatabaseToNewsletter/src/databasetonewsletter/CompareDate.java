b57baa85b0b0b2e083ab782b4cd5a351fae3bc46
Majitel <Majitel@147.32.219.19>
2013-04-18 14:00:15 +0200

    gi

--- DatabaseToNewsletter/src/databasetonewsletter/CompareDate.java
+++ DatabaseToNewsletter/src/databasetonewsletter/CompareDate.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package newsletterdatabase;
+package databasetonewsletter;
 
 import java.util.Comparator;
 
